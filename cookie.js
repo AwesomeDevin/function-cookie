75d5af579d97925e374e00d42de90c6a26b9186f
wb.dengwen <wb.dengwen@mesg.corp.netease.com>
2017-10-15 13:11:53 +0800

    fix

--- cookie.js
+++ cookie.js
@@ -1,1 +1,44 @@
-class Cookie{constructor(e){this.name=e}setCookie(e,t){let n;n="object"==typeof e?JSON.stringify(e):e;const o=new Date;o.setDate(o.getDate()+t),document.cookie=`${this.name}=${escape(n)}${null==t?"":`;expires=${o.toGMTString()}`}path=/`}getCookie(){if(document.cookie.length>0){let e=document.cookie.indexOf(`${this.name}=`);if(-1!==e){e=e+this.name.length+1;let t=document.cookie.indexOf(";",e);return-1===t&&(t=document.cookie.length),unescape(document.cookie.substring(e,t))}}return null}delCookie(){const e=new Date;e.setTime(e.getTime()-1),document.cookie=`${this.name}=0;expires=${new Date(0).toUTCString()}`}}export default Cookie;
+class Cookie {
+  constructor(name) {
+    this.name = name;
+  }
+
+  setCookie(value, expiredays) {
+    let cookieValue;
+    if (typeof (value) === 'object') {
+      cookieValue = JSON.stringify(value);
+    } else {
+      cookieValue = value;
+    }
+    const data = new Date();
+    data.setDate(data.getDate() + expiredays);
+    document.cookie = `${this.name}=${escape(cookieValue)
+}${(expiredays == null) ? '' : `;expires=${data.toGMTString()}`}`;
+  }
+
+  getCookie() {
+    console.log(document.cookie);
+    if (document.cookie.length > 0) {
+      let startIndex = document.cookie.indexOf(`${this.name}=`);
+      if (startIndex !== -1) {
+        startIndex = startIndex + this.name.length + 1;
+        let endIndex = document.cookie.indexOf(';', startIndex);
+        if (endIndex === -1) {
+          endIndex = document.cookie.length;
+        }
+        return unescape(document.cookie.substring(startIndex, endIndex));
+      }
+    }
+    return null;
+  }
+  
+   delCookie() {
+    console.log('delete');
+    const exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    document.cookie = `${this.name}=0;expires=${new Date(0).toUTCString()}`;
+  }
+}
+
+
+export default Cookie;
